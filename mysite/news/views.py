7bcde0dc717fb03b11b9d6426f7545d3d025628d
Denisitch <denisitch@gmail.com>
2021-08-07 16:04:20 +0300

    Templates

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from news.models import News
+
 
 def index(request):
-    # print(request)
-    return HttpResponse('Hello, World!')
-
-
-def test(request):
-    return HttpResponse('<h1>Test page</h1>')
+    news = News.objects.order_by('-created_at')
+    # res = '<h1>Список новостей</h1>'
+    # for item in news:
+    #     res += f'<div>\n<p>{item.title}</p>\n<p>{item.content}</p>\n<hr>\n'
+    # return HttpResponse(res)
+    context = {
+        'news': news,
+        'title': 'Список новостей'
+    }
+    return render(request, template_name='news/index.html', context=context)
