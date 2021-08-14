7cd605173d3893307afcc4c0aefacd2e9f64c151
Denisitch <denisitch@gmail.com>
2021-08-14 13:42:32 +0300

    Add form without model

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -3,13 +3,37 @@ from django.urls import reverse
 
 
 class News(models.Model):
-    title = models.CharField(max_length=150, verbose_name='Наименование')
-    content = models.TextField(blank=True, verbose_name='Контент')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
-    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
-    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
+    title = models.CharField(
+        max_length=150,
+        verbose_name='Наименование'
+    )
+    content = models.TextField(
+        blank=True,
+        verbose_name='Контент'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата публикации'
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Дата обновления'
+    )
+    photo = models.ImageField(
+        upload_to='photos/%Y/%m/%d/',
+        verbose_name='Фото',
+        blank=True
+    )
+    is_published = models.BooleanField(
+        default=True,
+        verbose_name='Опубликовано'
+    )
+    category = models.ForeignKey(
+        to='Category',
+        on_delete=models.PROTECT,
+        null=True,
+        verbose_name='Категория'
+    )
 
     def get_absolute_url(self):
         return reverse('view_news', kwargs={'news_id': self.pk})
@@ -24,7 +48,11 @@ class News(models.Model):
 
 
 class Category(models.Model):
-    title = models.CharField(max_length=150, db_index=True, verbose_name='Наименование категории')
+    title = models.CharField(
+        max_length=150,
+        db_index=True,
+        verbose_name='Наименование категории'
+    )
 
     def get_absolute_url(self):
         return reverse('category', kwargs={'category_id': self.pk})
