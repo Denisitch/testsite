67e12afad34dd7a1bac5db12c8d38a6717c4bd02
Denisitch <denisitch@gmail.com>
2021-08-15 20:27:18 +0300

    Add mixins

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,23 +1,25 @@
 from django.views.generic import ListView, DetailView, CreateView
 from news.models import News, Category
 from news.forms import NewsForm
+from news.utils import MyMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class HomeNews(ListView):
+class HomeNews(MyMixin, ListView):
     model = News
     template_name = 'news/index.html'
     context_object_name = 'news'
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(HomeNews, self).get_context_data(**kwargs)
-        context['title'] = 'Главная страница'
+        context['title'] = self.get_upper('Главная страница')
         return context
 
     def get_queryset(self):
         return News.objects.filter(is_published=True).select_related('category')
 
 
-class NewsByCategory(ListView):
+class NewsByCategory(MyMixin, ListView):
     model = Category
     template_name = 'news/index.html'
     context_object_name = 'news'
@@ -25,17 +27,19 @@ class NewsByCategory(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(NewsByCategory, self).get_context_data(**kwargs)
-        context['title'] = Category.objects.get(pk=self.kwargs['category_id'])
+        context['title'] = self.get_upper(Category.objects.get(pk=self.kwargs['category_id']))
         return context
 
     def get_queryset(self):
         return News.objects.filter(category_id=self.kwargs['category_id'], is_published=True).select_related('category')
 
 
-class ViewNews(DetailView):
+class ViewNews(LoginRequiredMixin, DetailView):
     model = News
     template_name = 'news/view_news.html'
     context_object_name = 'item_news'
+    # login_url = '/admin/'
+    raise_exception = True
 
 
 class CreateNews(CreateView):
