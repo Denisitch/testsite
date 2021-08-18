a1ef5e094799be55a986b82101a27f4c0363dbbe
Denisitch <denisitch@gmail.com>
2021-08-18 22:47:31 +0300

    Add registration

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,8 +1,29 @@
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView
 from news.models import News, Category
-from news.forms import NewsForm
+from news.forms import NewsForm, UserRegisterForm
 from news.utils import MyMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
+
+from django.contrib import messages
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Вы успешно зарегистрировались')
+            return redirect('login')
+        else:
+            messages.error(request, 'Ошибка регистрации')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'news/register.html', {'form': form})
+
+
+def login(request):
+    return render(request, 'news/login.html')
 
 
 class HomeNews(MyMixin, ListView):
